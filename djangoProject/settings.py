6edbbb2b34d15d373ee9467cb0ff990cd14eb912
Sus-arch <94695566+Sus-arch@users.noreply.github.com>
2022-09-25 14:31:20 +0300

    security update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f5g6dm8sm15l_a6&af(oxngv)dm2yr5erdt0%k2q8m#vw6as51'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'ANY_SECRET_KEY123')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
